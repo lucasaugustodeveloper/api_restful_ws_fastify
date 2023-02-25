3e44b5fa63e21d8ee2d0d22f5860e17d54958d53
Lucas Augusto <lucas.augusto5061@gmail.com>
2023-02-25 16:33:05 -0300

    add pino and pino-pretty for show better console log

--- src/index.js
+++ src/index.js
@@ -1,13 +1,29 @@
+/* eslint-disable import/no-extraneous-dependencies */
 require('dotenv').config();
 
-const fastify = require('fastify')({ logger: true });
+const envToLogger = {
+  development: {
+    transport: {
+      target: 'pino-pretty',
+      options: {
+        translate: 'HH:MM:ss Z',
+        ignore: 'pid,hostname',
+      },
+    },
+  },
+  production: true,
+  test: false,
+};
+
+const fastify = require('fastify')({ logger: envToLogger[process.env.NODE_ENV] });
+
 const routes = require('./http/routes');
 
 routes(fastify);
 
 const start = async () => {
   try {
-    await fastify.listen({ port: 3000 });
+    await fastify.listen({ port: process.env.APP_PORT });
   } catch (err) {
     fastify.log.error(err);
     process.exit(1);
